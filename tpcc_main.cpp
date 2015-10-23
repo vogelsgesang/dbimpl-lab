7c11431845a109edd1987b626c830f79685a9dff
Adrian Vogelsgesang <adrian.vogelsgesang@tum.de>
2015-10-23 16:13:28 +0200

    tpcc_main: print progress information

--- tpcc_main.cpp
+++ tpcc_main.cpp
@@ -11,27 +11,39 @@ using namespace std;
 int main() {
   auto tables = std::make_unique<table_data>();
   //load all the tables from disk
+  std::cout << "loading data tables" << std::endl;
   try {
     auto data_folder = std::string{"data/"};
     tables->warehouse.loadFromTbl(std::ifstream((data_folder + "tpcc_warehouse.tbl").c_str()));
+    std::cout << "." << std::flush;
     tables->district.loadFromTbl(std::ifstream((data_folder + "tpcc_district.tbl").c_str()));
+    std::cout << "." << std::flush;
     tables->customer.loadFromTbl(std::ifstream((data_folder + "tpcc_customer.tbl").c_str()));
+    std::cout << "." << std::flush;
     tables->history.loadFromTbl(std::ifstream((data_folder + "tpcc_history.tbl").c_str()));
+    std::cout << "." << std::flush;
     tables->neworder.loadFromTbl(std::ifstream((data_folder + "tpcc_neworder.tbl").c_str()));
+    std::cout << "." << std::flush;
     tables->order.loadFromTbl(std::ifstream((data_folder + "tpcc_order.tbl").c_str()));
+    std::cout << "." << std::flush;
     tables->orderline.loadFromTbl(std::ifstream((data_folder + "tpcc_orderline.tbl").c_str()));
+    std::cout << "." << std::flush;
     tables->item.loadFromTbl(std::ifstream((data_folder + "tpcc_item.tbl").c_str()));
+    std::cout << "." << std::flush;
     tables->stock.loadFromTbl(std::ifstream((data_folder + "tpcc_stock.tbl").c_str()));
+    std::cout << "." << std::flush;
   } catch(const char* e) {
     std::cout << e << std::endl;
     return -1;
   }
-
+  std::cout << "\ntpcc benchmark" << std::endl;
   clock_t begin_time = clock();
   auto iterations = 1'000'000;
   for(int i = 0; i < iterations; i++) {
     newOrderRandom(tables.get(), Timestamp(), 1);
+    if((i & 0xfff) == 0) std::cout << "." << std::flush;
   }
+  std::cout << std::endl;
   clock_t end_time = clock();
   double elapsed_secs = double(end_time - begin_time) / CLOCKS_PER_SEC;
   std::cout << (iterations / elapsed_secs) << " transactions per seconds" << std::endl;
